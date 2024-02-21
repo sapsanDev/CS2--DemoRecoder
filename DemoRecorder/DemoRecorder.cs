ea29ca9fc3593a76a4c5275b3a6814dcf07051cd
Sapsan <42625140+sapsanDev@users.noreply.github.com>
2024-02-21 20:58:52 +1000

    Update to v1.0.1
    Errors fixed.
    Added hook for ending demo recording

--- DemoRecorder/DemoRecorder.cs
+++ DemoRecorder/DemoRecorder.cs
@@ -1,65 +1,79 @@
-﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Config;
 using CounterStrikeSharp.API.Modules.Utils;
-using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
 using Microsoft.Extensions.Logging;
+using System.Runtime.InteropServices;
 
 namespace DemoRecorder;
-
+    
 public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
 {
     public override string ModuleName { get; } = "DemoRecorder";
-    public override string ModuleVersion { get; } = "1.0.0";
+    public override string ModuleVersion { get; } = "1.0.1";
     public override string ModuleAuthor { get; } = "SAPSAN";
+    public required PluginConfig Config { get; set; }
 
-    public PluginConfig Config { get; set; }
+    public List<CCSPlayerController> connectedPlayers = new();
 
-    public List<CCSPlayerController> connectedPlayers = new List<CCSPlayerController>();
+    private static string g_sDemosName = new (DateTime.Now.ToString("dd_MM_yyyy_HH_mm") + "-" + Server.MapName + ".dem"),
+                          g_sDemosDir = Server.GameDirectory;
 
-    public string g_sDemosName = "",
-                  g_sDemosDir = "",
-                  g_sServerName = "";
+    private static readonly string g_BinaryPath = g_sDemosDir + "/bin/" + (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "linuxsteamrt64/libengine2.so" : "win64/engine2.dll"),
+                         g_Signature = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) 
+                            ? @"\x55\x48\x89\xE5\x41\x57\x41\x56\x41\x55\x49\x89\xF5\x41\x54\x4C\x8D\x67\x08"
+                            : @"\x40\x55\x56\x41\x57\x48\x8D\x6C\x24\x00\x48\x81\xEC\x00\x00\x00\x00\x80\xB9\x00\x00\x00\x00\x00";
 
-    public bool g_bChangeMap, bOldState;
+    public bool g_bChangeMap, bOldState, g_bState;
+
+    public MemoryFunctionVoid<IntPtr, IntPtr> RecordEnd = new(g_Signature, g_BinaryPath);
+
+    private HookResult RecordEndHookResult(DynamicHook hook)
+    {
+        Task.Delay(1000).ContinueWith((task) =>
+        {
+            UploadDemo(g_sDemosDir + g_sDemosName, g_bState);
+        });
+        return HookResult.Continue;
+    }
 
     public void OnConfigParsed(PluginConfig config)
     {
         config = ConfigManager.Load<PluginConfig>(ModuleName);
         Config = config;
+
+        g_sDemosDir = Server.GameDirectory + "/csgo/addons/counterstrikesharp/data/" + Config.DemosDir;
+        CreateDemoDir();
     }
 
     public override void Load(bool hotReload)
     {
+        Directory.SetCurrentDirectory(Server.GameDirectory);
+
+        CreateDemoDir();
+
+        RecordEnd.Hook(RecordEndHookResult, HookMode.Post);
+
         RegisterEventHandler<EventCsIntermission>(OnEventCsIntermissionPost);
         RegisterListener<Listeners.OnMapStart>(OnMapStartHandler);
         RegisterListener<Listeners.OnMapEnd>(OnMapEndHandler);
-
-        g_sDemosDir = Directory.GetCurrentDirectory().Replace("bin/linuxsteamrt64", "");
-        Directory.SetCurrentDirectory(g_sDemosDir);
-
-        g_sServerName = ConVar.Find("hostname").StringValue;
-
-        g_sDemosDir += "/csgo/addons/counterstrikesharp/data/" + Config.DemosDir;
-
-        if (!Directory.Exists(g_sDemosDir))
-        {
-            Logger.LogInformation(">> Create folder for demos: {Folder}.", g_sDemosDir);
-
-            Directory.CreateDirectory(g_sDemosDir);
-        }
-        g_sDemosName = new string(DateTime.Now.ToString("dd_MM_yyyy_HH_mm") + "-" + Server.MapName + ".dem");
-
         UploadAllDemos();
+    }
+
+    public override void Unload (bool hotReload)
+    {
+        RecordEnd.Unhook(RecordEndHookResult, HookMode.Post);
     }
 
     [GameEventHandler(mode: HookMode.Post)]
     private HookResult OnEventCsIntermissionPost(EventCsIntermission @event, GameEventInfo info)
     {
-        RecordDemo(false, true);
+        g_bState = true;
+        RecordDemo(false);
         g_bChangeMap = true;
         return HookResult.Continue;
     }
@@ -84,7 +98,8 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
 
             if (GetActivePlayerCount() >= Config.MinOnline)
             {
-                RecordDemo(true, false);
+                g_bState = false;
+                RecordDemo(true);
             }
             return HookResult.Continue;
         }
@@ -92,7 +107,7 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
     }
 
     [GameEventHandler]
-    public  HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
     {
         var player = @event.Userid;
 
@@ -100,27 +115,40 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
 
         if (GetActivePlayerCount() < Config.MinOnline)
         {
-            RecordDemo(false, true);
+            g_bState = true;
+            RecordDemo(false);
             
         }
         return HookResult.Continue;
     }
+
     private void OnMapStartHandler(string mapName)
     {
         g_bChangeMap = false;
         g_sDemosName = new string(DateTime.Now.ToString("dd_MM_yyyy_HH_mm") + "-" + mapName + ".dem");
     }
 
+    private void CreateDemoDir()
+    {
+        if (!Directory.Exists(g_sDemosDir))
+        {
+            Logger.LogInformation(">> Create folder for demos: {Folder}.", g_sDemosDir);
+
+            Directory.CreateDirectory(g_sDemosDir);
+        }
+    }
+
     private void OnMapEndHandler()
     {
         if (!g_bChangeMap)
         {
-            RecordDemo(false, false);
+            g_bState = false;
+            RecordDemo(false);
             g_bChangeMap = true;
         }
     }
 
-    private void RecordDemo(bool bState, bool State)
+    private void RecordDemo(bool bState)
     {
         if (g_bChangeMap)
         {
@@ -141,10 +169,6 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
             Server.ExecuteCommand($"tv_stoprecord");
             Logger.LogInformation(">> Recording stop ({Name}).", g_sDemosName);
             
-            Task.Delay(800).ContinueWith((task) =>
-            {
-                UploadDemo(g_sDemosDir + g_sDemosName, State);
-            });
         }
     }
 
@@ -166,14 +190,13 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
         }
         catch (Exception ex)
         {
-            Logger.LogInformation(">> HttpPut Exception: {ex}", ex);
+            Logger.LogInformation(">> UploadDemo Exception: {ex}", ex);
         }
 
         if(bUploadOld)
         {
             UploadAllDemos();
         }
-        
     }
 
     public async Task<string> UploadFile(string path)
@@ -188,8 +211,6 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
                 using var req = new HttpRequestMessage(HttpMethod.Put, Config.UploadUrl);
                 {
                     req.Headers.Add("Auth", Config.Token);
-                    req.Headers.Add("Server-Name", g_sServerName);
-                    req.Headers.Add("Map-Name", path.Split('-').Last().Replace(".dem", ""));
                     req.Headers.Add("Demo-Name", path.Split('/').Last());
                     req.Headers.Add("Demo-ServerId", Config.ServerId.ToString());
                     req.Headers.Add("Demo-Time", File.GetCreationTime(path).ToString());
@@ -205,6 +226,7 @@ public class DemoRecorder : BasePlugin, IPluginConfig<PluginConfig>
             }
         }
     }
+
     void UploadAllDemos()
     {
         foreach (string file in Directory.GetFiles(g_sDemosDir))
